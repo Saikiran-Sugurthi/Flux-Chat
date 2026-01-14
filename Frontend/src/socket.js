0eb78009a2cd5691955fad028e02fddc049994d2
Sai Kiran Sugurthi <saikiranytg1@gmail.com>
2026-01-14 08:54:04 +0530

    fixing frontend for deployement

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+export const socket = io("https://flux-chat.onrender.com", {
+  transports: ["websocket"],
+});
